a81265e7d11b8216ca1a0d3d121ed7b1a96ee074
nikita <vasickinfedor@gmail.com>
2024-10-06 16:18:49 +0300

    H

---
+++ main.js
@@ -0,0 +1,23 @@
+const h = '4';
+console.log(h)
+
+
+//const b = 1; 
+//console.log(b + 1)
+//
+//if (h > b) {
+//  console.log('TRUE')
+//};
+
+//let c = console.log;
+//c("Pick it slow")
+
+
+
+
+//const $ = 4;
+//const hh = 1;
+//
+//console.log(hh + '$');
+//
+
